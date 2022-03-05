2f47054eb2f67b4acc19a2e98b7c520d5be4cc8c
Charlie Grevers <cgrevers@catalyte.io>
2022-03-05 17:18:18 -0500

    next

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -1,6 +1,10 @@
+/// <reference types="Cypress" />
+
 describe('My First Test Suite', () => {
     it('My First Test Case', () => {
         cy.visit('http://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.get('input.search-keyword').type('Cucumber')
+        cy.get('button.search-button').click()
     })
     // it('My Second Test Case', () => {
     // })
