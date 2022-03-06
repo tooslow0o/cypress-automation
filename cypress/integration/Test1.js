25b929318940d159dc9a726484146d77bf5c23d8
Charlie Grevers <cgrevers@catalyte.io>
2022-03-05 19:33:39 -0500

    redid this test better with wrap()

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -3,9 +3,44 @@
 describe('My First Test Suite', () => {
     it('My First Test Case', () => {
         cy.visit('http://rahulshettyacademy.com/seleniumPractise/#/')
-        cy.get('input.search-keyword').type('Cucumber')
+        // cy.get('input.search-keyword').type('Cucumber')
+        // cy.get('button.search-button').click()
+        // cy.get('.quantity').clear().type('69')
+        // cy.get('.product-action > button').click()
+        cy.get('input.search-keyword').type('ca')
         cy.get('button.search-button').click()
+        cy.get('div.product:visible').should('have.length', 4)
+        cy.get('div.product:hidden').should('have.length', 1)
+        cy.get('div.products').find('.product').should('have.length', 4)
+        cy.get('div.products').find('.product').eq(2).contains('ADD TO CART').click()
+        cy.get(':nth-child(3) > .product-action > button').click()
+
     })
-    // it('My Second Test Case', () => {
-    // })
+
+    it.only('My Second Test Case', () => {
+        cy.visit('http://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.get('input.search-keyword').type('ca')
+        cy.get('button.search-button').click()
+        //cy.pause()
+        // cy.get('h4.product-name').should('have.length', 4).as('productNames')
+
+        // cy.get('@productNames').each(($el) => {
+        //     let vegName = $el.text()
+        //     if (vegName.includes('Cashews')) {
+        //         cy.wrap($el).closest('.button').click()
+        //     }
+        // })
+
+        // cy.get('.product:visible').each(($el, index, $list) => {
+
+        cy.wait(2000)
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+
+            let textVeg = $el.find('h4.product-name').text()
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($el).find('.product-action>button').click()
+                // cy.wrap($el).closest('.button').click()
+            }
+        })
+    })
 })
